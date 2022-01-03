727375f41b183ebe653f7038aafc0b4110798448
Sangsaka Wira <sangsakawira@gmail.com>
2022-01-03 12:01:35 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,63 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var answer = "Belum Jawab";
+
+  answerQuestion(jawaban) {
+    setState(() {
+      answer = jawaban;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to Flutter v.1.0.1',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
         body: Column(
           children: [
-            Center(
+            const Center(
               child: Text("Apa ibukota negara Indonesia?"),
             ),
+            Center(
+              child: RaisedButton(
+                  child: const Text("A. Bandung"),
+                  onPressed: () {
+                    answerQuestion("Bandung");
+                  }),
+            ),
             RaisedButton(
-                child: Text("A. Bandung"),
-                onPressed: () => {print("A. Bandung")}),
+                child: const Text("B. DKI Jakarta"),
+                onPressed: () {
+                  answerQuestion("DKI Jakarta");
+                }),
             RaisedButton(
-                child: Text("B. DKI Jakarta"),
-                onPressed: () => {print("B. DKI Jakarta")}),
+                child: const Text("C. Surabaya"),
+                onPressed: () {
+                  answerQuestion("Surabaya");
+                }),
             RaisedButton(
-                child: Text("C. Surabaya"),
-                onPressed: () => {print("C. Surabaya")}),
-            RaisedButton(
-                child: Text("D. Medan"), onPressed: () => {print("D. Medan")})
+                child: const Text("D. Medan"),
+                onPressed: () {
+                  answerQuestion("Medan");
+                }),
+            Text("Jawaban Anda: " + answer)
           ],
         ),
       ),
